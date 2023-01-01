bd19a7d719971b282a2bd6974b089757caca7df2
StevesGitRepo <steve.k.szymanski@gmail.com>
2023-01-01 10:31:12 -0700

    site.js update

--- js/site.js
+++ js/site.js
@@ -4,27 +4,47 @@ const getValues = () => {
   // get values from the page
   let startValue = document.getElementById('startValue').value;
   let endValue = document.getElementById('endValue').value;
-  //we call generateNumbers
-  alert('The Start Value: ' + startValue);
-  //we call generateNumbers
-  let numbers = generateNumbers(startValue, endValue);
+
+  // We need to validate our input
+  //parse into Integers
+  startValue = parseInt(startValue);
+  endValue = parseInt(endValue);
+
+  if (Number.isInteger(startValue) && Number.isInteger(endValue)) {
+    //we call generateNumbers
+    let numbers = generateNumbers(startValue, endValue);
+    // we call displayNumbers
+    displayNumbers(numbers);
+  } else {
+    alert('You must enter integers');
+  }
 };
 
+//we call generateNumbers
+// numbers = generateNumbers(startValue, endValue);
 // generate numbers from startValue to the endValue
 // logic functions(s)
 
-const generateNumbers = (sValue, eValue) => {
-  let numbers = [];
+// const generateNumbers = (sValue, eValue) => {
+//   let numbers = [];
 
-  //get all numbers from start to end
+//get all numbers from start to end
 
-  for (let i = sValue; i <= eValue; i++) {
-    numbers.push(index);
-  }
+//   for (let index = sValue; index <= eValue; index++) {
+//     numbers.push(index);
+//   }
 
-  return numbers;
-};
+//   return numbers;
+// };
 
 // display the numbers and mark even numbers bold
 // display or view functions
-const displayNumbers = () => {};
+const displayNumbers = (numbers) => {
+  let templateRow = '';
+
+  for (let i = 0; i <= numbers.length; i++) {
+    let number = numbers[index];
+  }
+
+  document.getElementById('results').innerHTML = templateRows;
+};
