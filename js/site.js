c4123fcbec6b71e3cdfa5a99fd5eb9d22aac4059
StevesGitRepo <steve.k.szymanski@gmail.com>
2023-01-02 15:52:25 -0700

    Code page summary added
    Javascript code explanation added

--- js/site.js
+++ js/site.js
@@ -1,17 +1,16 @@
-// get the values from the Page
-// starts our controller function
-function getValues() {
+// get the values from the page, starts our controller function
+const getValues = () => {
   // get values from the page
   let startValue = document.getElementById('startValue').value;
   let endValue = document.getElementById('endValue').value;
   let numbers = generateNumbers(startValue, endValue);
 
-  // alert('The Start Value is ' + startValue);
-  // alert('The End Value is ' + endValue);
   // We need to validate our input
   //parse into Integers
   startValue = parseInt(startValue);
   endValue = parseInt(endValue);
+
+  // this script checks for integers, run before and after the parseInt:
   // alert('The Combined Value is ' + (startValue + endValue));
 
   if (Number.isInteger(startValue) && Number.isInteger(endValue)) {
@@ -20,20 +19,30 @@ function getValues() {
     alert('You must enter integers');
   }
 
+  // these scripts alert the user that they are exceeding the scope of the project
+  if (startValue < -99 || endValue > 199) {
+    alert('ERROR: DOES NOT COMPUTE');
+    return false;
+  }
+  if (startValue < 1 || endValue > 100) {
+    alert('This still works but try not to break the internet, ok?');
+  }
+
   // we call displayNumbers
   displayNumbers(numbers);
-}
+};
 
-function generateNumbers(sValue, eValue) {
+const generateNumbers = (sValue, eValue) => {
   let numbers = [];
+
   for (let index = sValue; index <= eValue; index++) {
     numbers.push(index);
   }
 
   return numbers;
-}
+};
 
-function displayNumbers(numbers) {
+const displayNumbers = (numbers) => {
   let templateRows = '';
 
   for (let index = 0; index < numbers.length; index++) {
@@ -48,6 +57,7 @@ function displayNumbers(numbers) {
 
     templateRows += `<tr><td class="${className}">${number}</td></tr>`;
   }
-
+  // the following console.log script is used for debugging
+  // console.log(numbers);
   document.getElementById('results').innerHTML = templateRows;
-}
+};
